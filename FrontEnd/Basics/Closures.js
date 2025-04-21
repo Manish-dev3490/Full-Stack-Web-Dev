4d99f56c740a7b224fa1293a4da73197392c58e7
manish-dev3490 <kmanish87064@gmail.com>
2025-04-21 19:32:28 +0530

    interview problem related t clousres

---
+++ FrontEnd/Basics/Closures.js
@@ -0,0 +1,11 @@
+// In this file i am going to learn about closures and scopes...
+
+    
+    for(var a=1;a<=6;a++){
+        var i=1;
+        setTimeout(function(){
+            console.log(i);
+            i++;  
+        },a*1000)
+       
+    }
