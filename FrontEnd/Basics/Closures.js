610e2a060db3dc03ff127641f2ad2aeeca8af167
manish-dev3490 <kmanish87064@gmail.com>
2025-04-22 15:13:34 +0530

    closures

--- FrontEnd/Basics/Closures.js
+++ FrontEnd/Basics/Closures.js
@@ -1,11 +1,13 @@
 // In this file i am going to learn about closures and scopes...
 
-    
-    for(var a=1;a<=6;a++){
-        var i=1;
-        setTimeout(function(){
-            console.log(i);
-            i++;  
-        },a*1000)
-       
+    function x(){
+        for(var a=1;a<=6;a++){
+            var i=1;
+            setTimeout(function(){
+                console.log(i);
+                i++;  
+            },a*1000)
+           
+        }
     }
+   x();
