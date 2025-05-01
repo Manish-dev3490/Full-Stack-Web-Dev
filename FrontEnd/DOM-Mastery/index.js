ba6016f50b6cfffc26e141ebf4917747559b88ff
manish-dev3490 <kmanish87064@gmail.com>
2025-05-01 17:12:17 +0530

    delete the todo list from the main list feauture is implemented

--- FrontEnd/DOM-Mastery/index.js
+++ FrontEnd/DOM-Mastery/index.js
@@ -3,11 +3,33 @@ const queryArea=document.getElementById("query-area");
 const addBtn=document.getElementById("add-btn");
 
 function addTaskInList(){
+    let value=queryArea.value;
     const list=document.createElement("li");
     list.classList.add("todo-list")
-    const editBtn=document.createElement("button");
-    const deleteBtn=document.createElement("button");
+    list.innerHTML=`
+      <input type="checkbox" class="checkbox">
+                    <input type="text" name="" class="list" value="${value}">
+                    <input type="button" id="edit-btn"  value="Edit">
+                    <input type="button" id="delete-btn" value="Delete">
+    `;
+    if(value){
+    taskList.appendChild(list);
+    queryArea.value=null;
+    }
 
+    else {
+        window.alert("Please enter your query");
+    }
+
+
+
+    list.addEventListener("click",function(event){
+        if(event.target.id=="delete-btn"){
+            event.target.parentElement.remove();
+        }
+        
+        
+    })
 
 
 }
