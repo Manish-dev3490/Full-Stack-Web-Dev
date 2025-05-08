12927b0def454770f5f418041484b6c50f6191ca
manish-dev3490 <kmanish87064@gmail.com>
2025-05-08 19:46:39 +0530

    practice of asycn await

--- FrontEnd/Async-JS/practice_Promise.js
+++ FrontEnd/Async-JS/practice_Promise.js
@@ -1,28 +1,28 @@
 let n="Manish";
 
 // Produced a new promise and handling promise in oler fashion
-// const prom=new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         resolve("Your promise has been resolved");
-//     },5000)
-// })
+const prom=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve("Your promise has been resolved");
+    },5000)
+})
 
 
 
-// function handlePromise(){
-//     prom.then(function(retrunval){
-//         console.log(retrunval);
+function handlePromise(){
+    prom.then(function(retrunval){
+        console.log(retrunval);
         
-//     }).catch(function(error){
-//         console.log(error);
+    }).catch(function(error){
+        console.log(error);
         
-//     })
+    })
 
-//     console.log('Namastey javascript developers ko ');
+    console.log('Namastey javascript developers ko ');
     
-// }
+}
 
-// handlePromise();
+handlePromise();
 
 
 
@@ -32,14 +32,26 @@ let n="Manish";
 const promi=new Promise(function(res,rej){
     setTimeout(() => {
         res("Your promise is reolved");
-    }, 4000);
+    }, 9000);
 })
 
+const promi2=new Promise(function(res,rej){
+    setTimeout(() => {
+        res("Your promise is reolved");
+    }, 5000);
+})
+ 
 
  async function handlePromi(){
+    console.log('helllo before ');
+    
     const data=await promi;
     console.log('Namastey js devs');
     console.log(data);
+
+    const data2=await promi2;
+    console.log('Namastey js devs 2');
+    console.log(data2);
 }
 
 handlePromi();
