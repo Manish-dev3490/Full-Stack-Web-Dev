6e22deeb5c3ae93d51aaa37ad0dbec56684d6b92
manish-dev3490 <kmanish87064@gmail.com>
2025-05-08 18:58:34 +0530

    Async await

--- FrontEnd/Async-JS/practice_Promise.js
+++ FrontEnd/Async-JS/practice_Promise.js
@@ -1,25 +1,45 @@
 let n="Manish";
 
 // Produced a new promise and handling promise in oler fashion
-const prom=new Promise(function(resolve,reject){
-    setTimeout(function(){
-        resolve("Your promise has been resolved");
-    },5000)
+// const prom=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         resolve("Your promise has been resolved");
+//     },5000)
+// })
+
+
+
+// function handlePromise(){
+//     prom.then(function(retrunval){
+//         console.log(retrunval);
+        
+//     }).catch(function(error){
+//         console.log(error);
+        
+//     })
+
+//     console.log('Namastey javascript developers ko ');
+    
+// }
+
+// handlePromise();
+
+
+
+
+// Producing a promise and handling it with new pattern using async await
+
+const promi=new Promise(function(res,rej){
+    setTimeout(() => {
+        res("Your promise is reolved");
+    }, 4000);
 })
 
 
-
-function handlePromise(){
-    prom.then(function(retrunval){
-        console.log(retrunval);
-        
-    }).catch(function(error){
-        console.log(error);
-        
-    })
-
-    console.log('Namastey javascript developers ko ');
-    
+ async function handlePromi(){
+    const data=await promi;
+    console.log('Namastey js devs');
+    console.log(data);
 }
 
-handlePromise();
+handlePromi();
