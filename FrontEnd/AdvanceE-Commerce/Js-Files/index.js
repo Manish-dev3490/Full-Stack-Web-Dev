ad7a4a0117986241ca590ac515f1ce3ff2a4e2cb
manish-dev3490 <kmanish87064@gmail.com>
2025-05-12 16:28:27 +0530

    Homepage is created with styling

---
+++ FrontEnd/AdvanceE-Commerce/Js-Files/index.js
@@ -0,0 +1,7 @@
+const logoutBtn=document.getElementById("logout-bt");
+const mainContainer=document.getElementsByClassName("Maincontainer");
+
+
+window.addEventListener("load", async function(){
+
+})
