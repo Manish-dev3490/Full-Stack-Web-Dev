98aef9601fd022b33e051648d5ac6847b3fba1dc
manish-dev3490 <kmanish87064@gmail.com>
2025-05-12 18:04:20 +0530

    Category list structure  with styling logic is created

--- FrontEnd/AdvanceE-Commerce/Js-Files/category.js
+++ FrontEnd/AdvanceE-Commerce/Js-Files/category.js
@@ -1,17 +1,30 @@
 //  In this file i am going to consume category apis
-const categoryContainer=document.getElementsByClassName("category-container");
+const categoryContainer = document.querySelector(".category-container");
+
+
 // Get the category from the URL
 const params = new URLSearchParams(window.location.search);
 const category = params.get('category');
 
-console.log(category); // Output: "women-bags"
 
 
-window.addEventListener("load",async function(){
-const data=await fetch(`https://dummyjson.com/products/category/${category}`);
-const response=await data.json();
-console.log(response);
+window.addEventListener("load", async function () {
+    const data = await fetch(`https://dummyjson.com/products/category/${category}`);
+    const response = await data.json();
+    console.log(response);
 
+    response.products.map(function (data) {
+        const product = document.createElement("div");
+        product.classList.add("product-gallary");
+        product.innerHTML = `
+      <a href="">
+   <img src="${data.images[0]}" loading="lazy">
+ </a>
+   <p>${data.description}</p>
+   <p class="price-tag">Rs. <span class="span">${data.price}</span></p>
+    `;
+        categoryContainer.appendChild(product);
 
+    })
 
 })
