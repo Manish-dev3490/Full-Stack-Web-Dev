df92b474fc2dd00ca7ab020344ee8d3a16a97831
manish-dev3490 <kmanish87064@gmail.com>
2025-05-14 21:01:21 +0530

    Filter feauture is  implemented in the web application

--- FrontEnd/AdvanceE-Commerce/Js-Files/AllProduct.js
+++ FrontEnd/AdvanceE-Commerce/Js-Files/AllProduct.js
@@ -11,6 +11,9 @@ const userName = document.querySelector(".user-name");
 const userFirstName = localStorage.getItem("userFirstName");
 const userLastName = localStorage.getItem("userLastName");
 
+const applyFilter = document.querySelector(".apply-filter");
+
+
 let skip = 10;
 
 window.addEventListener("load", async function () {
@@ -29,6 +32,41 @@ window.addEventListener("load", async function () {
 
   else userName.textContent = userEmail;
   cartLength.textContent = `CartList (${cartItems.length})`;
+
+
+  const allData=await fetch("https://dummyjson.com/products?limit=100");
+  const allResponse=await allData.json();
+
+applyFilter.addEventListener("click",function(){
+const minPrice = document.querySelector("#min-price").value;
+const maxPrice = document.querySelector("#max-price").value;
+
+const filteredData=allResponse.products.filter(function(data){
+return data.price>minPrice && data.price<maxPrice;
+})
+console.log(filteredData);
+
+productContainer.innerHTML="";
+ filteredData.map(function (data) {
+    const product = document.createElement("div");
+    product.classList.add("product-gallary");
+    product.innerHTML = `
+      <a href="../pages/Product_Detail.html?id=${data.id}">
+   <img src="${data.images[0]}">
+ </a>
+   <p class="desc">${data.description}</p>
+   <p class="price-tag">Rs. <span class="span">${data.price}</span></p>
+    `;
+    productContainer.appendChild(product);
+
+  })
+
+
+})
+
+
+
+
 
   userLogo.addEventListener("click", function () {
     const currentDisplay = getComputedStyle(logOutButton).display;
@@ -51,7 +89,7 @@ window.addEventListener("load", async function () {
 
 
 
- 
+
   const wholeData = await fetch('https://dummyjson.com/products?limit=10&skip=10');
   const response2 = await wholeData.json();
 
