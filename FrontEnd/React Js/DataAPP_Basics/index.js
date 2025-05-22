0eff6a5c58044e2958c0193497052fc7aba326e5
manish-dev3490 <kmanish87064@gmail.com>
2025-05-22 20:33:55 +0530

    Created a small basic react js aplication

--- FrontEnd/React Js/DataAPP_Basics/index.js
+++ FrontEnd/React Js/DataAPP_Basics/index.js
@@ -1,5 +1,28 @@
 import React from "react";
-import ReactDom from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-console.log(ReactDom);
+// Created root where I have to append my container
+const root = ReactDOM.createRoot(document.querySelector(".root"));
+GetDataFromServer();
 
+function makeCard(id, title, price, image) {
+    return (
+        <div key={id} style={{ border: "1px solid gray", padding: "10px", margin: "10px", width: "200px" }}>
+            <img src={image} alt={title} style={{ width: "100%", height: "150px", objectFit: "cover" }} />
+            <h2>{title}</h2>
+            <h4>₹{price}</h4>
+        </div>
+    );
+}
+
+async function GetDataFromServer() {
+    const data = await fetch('https://dummyjson.com/products');
+    const response = await data.json();
+
+    const container2 = response.products.map(function (card) {
+        return makeCard(card.id, card.title, card.price, card.images[0]);
+    });
+
+    // Render the container2 inside a parent container
+    root.render(<div style={{ display: "flex", flexWrap: "wrap" }}>{container2}</div>);
+}
