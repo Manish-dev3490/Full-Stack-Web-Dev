d13e852333b3fb30ad8b706e52098d1ef4b62070
manish-dev3490 <kmanish87064@gmail.com>
2025-05-24 14:21:47 +0530

    Deleted that structure

--- FrontEnd/React Js/CounterApp/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-
-// This is the root of our react js application
-const root = ReactDOM.createRoot(document.querySelector(".root"));
