a807fd28e3b8007ca5bf7edfb9a703cc53dbc006
manish-dev3490 <kmanish87064@gmail.com>
2025-05-24 13:14:07 +0530

    Created a proper organised  folder structure

---
+++ FrontEnd/React Js/CounterApp/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Creating main app component for our application
+const App = function () {
+    return (
+        <p>Hello world </p>
+    )
+}
+
+export default App;
--- FrontEnd/React Js/CounterApp/index.js
+++ FrontEnd/React Js/CounterApp/index.js
@@ -1,3 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
+// This is the root of our react js application
+const root = ReactDOM.createRoot(document.querySelector(".root"));
