298104eb5df02375f2a7431849b677843bc92928
manish-dev3490 <kmanish87064@gmail.com>
2025-05-29 12:30:53 +0530

    just learning how react re render the component when state of the component changed

--- FrontEnd/React Js/CountApplication/my-vite-app/src/Components/AppleContainer.jsx
+++ FrontEnd/React Js/CountApplication/my-vite-app/src/Components/AppleContainer.jsx
@@ -3,55 +3,68 @@ import LeftArrow from "../assets/left.png";
 import RightArrow from "../assets/right.png";
 import Basket from "./Basket";
 
+import { createRoot } from "react-dom/client";
 
 
-
+  const totalApples = 10;
+  let leftapples = totalApples - 5;
+  let rightApples = totalApples - leftapples;
 const AppleContainer = function () {
 
 
-    const totalApples = 10;
-    let leftapples = totalApples - 5;
-    let rightApples = totalApples - leftapples;
+  function leftclickHandler() {
+    console.log("Left arrow");
+    rightApples++;
+    leftapples--;
+    console.log(rightApples);
+    console.log(leftapples);
+  }
 
-    function leftclickHandler() {
-        console.log("Left arrow");
-        rightApples++;
-        leftapples--;
-        console.log(rightApples);
-        console.log(leftapples);
+  function rightclickHandler() {
+    console.log("Right arrow");
+    rightApples--;
+    leftapples++;
 
+    console.log(rightApples);
+    console.log(leftapples);
+  }
 
-    }
+  return (
+    <>
+      <div
+        className="AppleContainer"
+        style={{
+          marginInline: "6rem",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Basket count={totalApples} />
 
-    function rightclickHandler() {
-        console.log("Right arrow");
-        rightApples--;
-        leftapples++;
+        <Button
+          clickHandler={leftclickHandler}
+          imageUrl={LeftArrow}
+          tit="left-arrow"
+        />
+        <Button
+          clickHandler={rightclickHandler}
+          imageUrl={RightArrow}
+          tit="rightt-arrow"
+        />
+        <Basket count={leftapples} />
+      </div>
 
-        console.log(rightApples);
-        console.log(leftapples);
-    }
-
-    return (
-        <div
-            className="AppleContainer"
-            style={{
-                marginInline: "6rem",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-            }}
-        >
-            <Basket count={totalApples} />
-
-            <Button clickHandler={leftclickHandler} imageUrl={LeftArrow} tit="left-arrow" />
-            <Button
-                clickHandler={rightclickHandler}
-                imageUrl={RightArrow}
-                tit="rightt-arrow"
-            />
-            <Basket count={0} />
-        </div>
-    );
+      <button
+        onClick={function () {
+          createRoot(document.getElementById("root")).render(
+            <AppleContainer />
+          );
+        }}
+      >
+        Re Render
+      </button>
+    </>
+  );
 };
 export default AppleContainer;
