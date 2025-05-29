aaf53b3755a0d19b28a1b45173a1a0ce31ac18ed
manish-dev3490 <kmanish87064@gmail.com>
2025-05-29 11:44:14 +0530

    trying to implement event listener functionality

--- FrontEnd/React Js/CountApplication/my-vite-app/src/Components/AppleContainer.jsx
+++ FrontEnd/React Js/CountApplication/my-vite-app/src/Components/AppleContainer.jsx
@@ -1,27 +1,57 @@
 import Button from "./Button";
-import LeftArrow from '../assets/left.png';
-import RightArrow from '../assets/right.png';
+import LeftArrow from "../assets/left.png";
+import RightArrow from "../assets/right.png";
 import Basket from "./Basket";
 
+
+
+
 const AppleContainer = function () {
+
+
+    const totalApples = 10;
+    let leftapples = totalApples - 5;
+    let rightApples = totalApples - leftapples;
+
+    function leftclickHandler() {
+        console.log("Left arrow");
+        rightApples++;
+        leftapples--;
+        console.log(rightApples);
+        console.log(leftapples);
+
+
+    }
+
+    function rightclickHandler() {
+        console.log("Right arrow");
+        rightApples--;
+        leftapples++;
+
+        console.log(rightApples);
+        console.log(leftapples);
+    }
+
     return (
-        <div className="AppleContainer" style={{ marginInline: '6rem', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-            <Basket count={1} />
+        <div
+            className="AppleContainer"
+            style={{
+                marginInline: "6rem",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+            }}
+        >
+            <Basket count={totalApples} />
 
-            <Button clickHandler={function () {
-                console.log("Light arrow");
-
-            }} imageUrl={LeftArrow} tit='left-arrow' />
-            <Button clickHandler={
-                function () {
-                    console.log("right aroow");
-
-                }
-            } imageUrl={RightArrow} tit='rightt-arrow' />
-            <Basket count={2} />
-
-
+            <Button clickHandler={leftclickHandler} imageUrl={LeftArrow} tit="left-arrow" />
+            <Button
+                clickHandler={rightclickHandler}
+                imageUrl={RightArrow}
+                tit="rightt-arrow"
+            />
+            <Basket count={0} />
         </div>
-    )
-}
+    );
+};
 export default AppleContainer;
