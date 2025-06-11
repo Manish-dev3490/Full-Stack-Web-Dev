6cebfa9a98ea8f1d59a7171a5c6469ac6673d9df
manish-dev3490 <kmanish87064@gmail.com>
2025-06-11 15:15:21 +0530

    react and ReactDOM behind the scenes

--- FrontEnd/React Js/BaseofReact/index.js
+++ FrontEnd/React Js/BaseofReact/index.js
@@ -1,42 +1,36 @@
-// In this file i am going to learn how react is actually working behind the scene we know there are two ways to get react into our project with using cdn links and using npm || yarn.but understand how it works behind the scene .so when we get reactinto our code whole code of react get into our file and we use that let me show you how it actually works
-
-// Problem 1 -> we have to create a element h2 we will create it with docuemnt.createElement("h2") and it is object at the end of the day we will put innerText style using . dot operator but it is very lengthy and hectic and code becaomes very unreadable so let us learn with react
-
-// Solution below the line this is how react code is written in the react files that we use from cdn and using npm
+// This is how actual code of react is written behind the scenes by the facebook engineers
 const React = {
-  // This is how actual code of react is written behind the scenes by the facebook engineers
   createElement: function (tagName, styles, children) {
     const element = document.createElement(tagName);
 
-    // Now we will recieve styles properties we have to apply style on element but ek ek karke lagana is not possible because bhut saari properties ho skti hai so use loop
-
-    // element.style.fontSize=styles.fontSize;
-    // element.style.backgroundColor=styles.backgroundColor;
-
-
-    // Use forn in loop for iterating over the objects
     for (let key in styles) {
-        element.style[key]=styles[key];
+      element.style[key] = styles[key];
     }
-
-    // jo bhi children aayega usko hum element me daal denge
     element.innerText = children;
     return element;
   },
 };
 
+// Behind the scenes of library RECATDOM
+const ReactDOM = {
+  render: function (element, root) {
+    root.append(element);
+  }
+}
+
 // Now create element by using React.createElement
-const h1 = React.createElement(
-  "h1",
-  { fontSize: "30px", backgroundColor: "red" ,color:"blue"},
-  "hello to the world"
-);
-const h2 = React.createElement(
-  "h2",
-  { fontSize: "20px", backgroundColor: "green" },
-  "hello again to the world"
+const h1 = React.createElement("h1", { fontSize: "30px", backgroundColor: "red", color: "blue" }, "hello to the world");
+const h2 = React.createElement("h2", { fontSize: "20px", backgroundColor: "green" }, "hello again to the world"
 );
 
-const root = document.getElementById("root");
-root.append(h1);
-root.append(h2);
+
+// we are appending element in rot using javascript lets append using REACTDOM
+// const root = document.getElementById("root");
+// root.append(h1);
+// root.append(h2);
+
+
+
+// we will render the elements using ReactDOM
+ReactDOM.render(h1,document.getElementById("root"));
+ReactDOM.render(h2,document.getElementById("root"));
