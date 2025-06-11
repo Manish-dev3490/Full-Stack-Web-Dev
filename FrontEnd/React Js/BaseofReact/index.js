11cf854cd68b6f4120d58ad4247756c25b42dc44
manish-dev3490 <kmanish87064@gmail.com>
2025-06-11 15:34:19 +0530

    created my own react library by using javascripts

--- FrontEnd/React Js/BaseofReact/index.js
+++ FrontEnd/React Js/BaseofReact/index.js
@@ -3,10 +3,23 @@ const React = {
   createElement: function (tagName, styles, children) {
     const element = document.createElement(tagName);
 
+    // looping over css styles
     for (let key in styles) {
       element.style[key] = styles[key];
     }
-    element.innerText = children;
+
+    // if children is array
+    if (typeof children === 'object') {
+      for (let val of children) {
+        element.append(val);
+      }
+    }
+
+    // if children is string
+    else {
+      element.innerText = children;
+
+    }
     return element;
   },
 };
@@ -20,8 +33,7 @@ const ReactDOM = {
 
 // Now create element by using React.createElement
 const h1 = React.createElement("h1", { fontSize: "30px", backgroundColor: "red", color: "blue" }, "hello to the world");
-const h2 = React.createElement("h2", { fontSize: "20px", backgroundColor: "green" }, "hello again to the world"
-);
+const h2 = React.createElement("h2", { fontSize: "20px", backgroundColor: "green" }, "hello again to the world");
 
 
 // we are appending element in rot using javascript lets append using REACTDOM
@@ -32,5 +44,13 @@ const h2 = React.createElement("h2", { fontSize: "20px", backgroundColor: "green
 
 
 // we will render the elements using ReactDOM
-ReactDOM.render(h1,document.getElementById("root"));
-ReactDOM.render(h2,document.getElementById("root"));
+ReactDOM.render(h1, document.getElementById("root"));
+ReactDOM.render(h2, document.getElementById("root"));
+
+
+// Now think if we have to make a ul and 3*li -->html css js how will we create that lets us see
+const l1 = React.createElement("li", {}, "HTML");
+const l2 = React.createElement("li", {}, "CSS");
+const l3 = React.createElement("li", {}, "JS");
+const ul = React.createElement("ul", { fontSize: "30px", backgroundColor: "red", color: "blue" }, [l1, l2, l3]);
+ReactDOM.render(ul, document.getElementById("root"));
