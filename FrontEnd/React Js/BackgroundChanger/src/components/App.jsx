749c1c4b906b9083f91fec9243ef2bd51984326e
manish-dev3490 <kmanish87064@gmail.com>
2025-06-19 17:17:26 +0530

    created a ignited react applicatuion from scratch and setuped two components

---
+++ FrontEnd/React Js/BackgroundChanger/src/components/App.jsx
@@ -0,0 +1,10 @@
+
+
+// This is the whole react-appication all the components will dig under this App component
+function App() {
+    return (
+        <div>App</div>
+    )
+}
+
+export default App
