c6cc9f59d352e0090be94f14e9f032f09873778b
manish-dev3490 <kmanish87064@gmail.com>
2025-07-10 02:07:28 +0530

    updated some comments

--- FrontEnd/React Js/ContextAPI/App.jsx
+++ FrontEnd/React Js/ContextAPI/App.jsx
@@ -3,7 +3,7 @@ import Second from './components/Second';
 import Third from './components/Third';
 
 
-// So in this file what i am doing creating a state variable and passing to my children and my children is passing to its children this type of chain is known technically props drilling.
+// So in this file what i am doing creating a state variable and passing to my children and my children is passing to its children this type of chain is known technically props drilling.so problem is assume there are nested children and main component se kuch data bhejna hai end vale children ko jo beech ke hai unke uska koi use nahi hai lekin unke trhough paas hora hai isko hee props drilling kahte hai it is a problem which is solved by useContext Api .
 
 function App() {
     const [count, setcount] = useState(0);
