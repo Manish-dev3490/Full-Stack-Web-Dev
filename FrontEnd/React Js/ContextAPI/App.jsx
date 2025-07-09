1a90c5747ac44fff0c78cc58e68a93c31396d9b0
manish-dev3490 <kmanish87064@gmail.com>
2025-07-10 02:02:38 +0530

    learned about props drilling

--- FrontEnd/React Js/ContextAPI/App.jsx
+++ FrontEnd/React Js/ContextAPI/App.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import Second from './components/Second';
 import Third from './components/Third';
 
+
+// So in this file what i am doing creating a state variable and passing to my children and my children is passing to its children this type of chain is known technically props drilling.
+
 function App() {
     const [count, setcount] = useState(0);
     return (
