cf2f6ad43b1343dbdac953cc6d63905327c94a5b
manish-dev3490 <kmanish87064@gmail.com>
2025-07-10 21:23:34 +0530

    understood about react router routes,route,links

--- FrontEnd/React Js/React-Router/Components/App.jsx
+++ FrontEnd/React Js/React-Router/Components/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router';
+import Home from './Home';
+import About from './About'
+import Contact from './Contact'
+import Help from './Help'
+
 
 function App() {
-  return (
-    <div>
-        welcome to the App
-    </div>
-  )
+    return (
+        <BrowserRouter>
+
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/About">About</Link>
+                <Link to="/Contact">Contact</Link>
+                <Link to="/Help">Help</Link>
+
+            </nav>
+
+            <Routes>
+                <Route path='/' element={<Home></Home>} ></Route>
+                <Route path='/About' element={<About></About>} ></Route>
+                <Route path='/Contact' element={<Contact></Contact>} ></Route>
+                <Route path='/Help' element={<Help></Help>} ></Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
