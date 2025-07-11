fb7a105e0b55ce721561a4fd1e93ae705b1d256e
manish-dev3490 <kmanish87064@gmail.com>
2025-07-11 19:04:36 +0530

    created new components

---
+++ FrontEnd/Redux/State_Management/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Counter from './components/Counter'
+
+function App() {
+  return (
+    <div>
+      <h2>Welcome to the App Level Component</h2>
+      <Counter></Counter>
+    </div>
+  )
+}
+
+export default App
---
+++ FrontEnd/Redux/State_Management/src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Counter() {
+    return (
+        <div>
+            <h2>Count is: </h2>
+            <button>Increase the count</button>
+            <button>Decrease the count</button>
+        </div>
+    )
+}
+
+export default Counter
