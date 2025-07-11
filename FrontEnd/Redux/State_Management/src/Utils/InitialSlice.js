c3517d7564ae7e68722c0917f4c5e16567c7e1fa
manish-dev3490 <kmanish87064@gmail.com>
2025-07-11 20:11:09 +0530

    created a slice

---
+++ FrontEnd/Redux/State_Management/src/Utils/InitialSlice.js
@@ -0,0 +1,13 @@
+import createSlice from '@reduxjs/toolkit';
+
+const firstSlice = createSlice({
+    name: "pehliSlice",
+    initialState: {
+        count: 0
+    },
+    reducers: {
+        Increment: (state) => { state.count = state.count + 1 },
+        Decrement: (state) => { state.count = state.count - 1 },
+        Reset: (state) => { state.count = 0 },
+    }
+})
