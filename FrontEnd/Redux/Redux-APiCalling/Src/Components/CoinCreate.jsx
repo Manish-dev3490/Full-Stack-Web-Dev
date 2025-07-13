bad44fec80fcd728c25e49144dcdd04589379495
manish-dev3490 <kmanish87064@gmail.com>
2025-07-13 20:11:41 +0530

    fetched and rendered data

--- FrontEnd/Redux/Redux-APiCalling/Src/Components/CoinCreate.jsx
+++ FrontEnd/Redux/Redux-APiCalling/Src/Components/CoinCreate.jsx
@@ -1,23 +1,38 @@
-import React, { useEffect } from 'react'
-import {useSelector,useDispatch} from 'react-redux'
-import { fetchCyrpto } from '../Utils/Slice1';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCyrpto } from "../Utils/Slice1"; // fixed typo
 
 function CoinCreate() {
-    const {data,loading,error}=useSelector((store)=>store?.slice1);
-    const dispatch=useDispatch();
-    
+    const { data, loading, error } = useSelector((store) => store?.slice1);
+    const dispatch = useDispatch();
 
-    useEffect(()=>{
-        dispatch(fetchCyrpto(20));
-        console.log(data);
-        
-    },[])
+    useEffect(() => {
+        dispatch(fetchCyrpto(20)); // fixed typo
+    }, [dispatch]);
 
-  return (
-    <div>
+    if (loading) return <h2>Data is loading...</h2>;
+    if (error) return <h2>Something went wrong!</h2>;
 
-    </div>
-  )
+    return (
+        <div style={{
+            display: flex,
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+            gap: '20px',
+            marginBlock: '15px'
+        }}>
+            {data?.map((coin, index) => (
+                <div key={coin.id}>
+                    <h3>{coin.name}</h3>
+                    <p>Price: {coin.price}</p>
+                    <img src={coin?.image} alt={coin?.name} />
+
+
+                </div>
+            ))}
+        </div>
+    );
 }
 
-export default CoinCreate
+export default CoinCreate;
