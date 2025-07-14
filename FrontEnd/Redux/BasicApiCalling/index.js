d755d321e9166849b223aa98afef6f8cab89559e
manish-dev3490 <kmanish87064@gmail.com>
2025-07-15 00:21:07 +0530

    created a redux slice and going to create store

--- FrontEnd/Redux/BasicApiCalling/index.js
+++ FrontEnd/Redux/BasicApiCalling/index.js
@@ -4,4 +4,4 @@ import App from "./src/components/App";
 
 
 // Rendering the App component into the root location
-ReactDOM.createRoot(document.querySelector("root")).render(<App></App>)
+ReactDOM.createRoot(document.querySelector(".root")).render(<App></App>)
---
+++ FrontEnd/Redux/BasicApiCalling/src/components/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Body() {
+    
+  return (
+    <div>
+        {
+
+        }
+    </div>
+  )
+}
+
+export default Body
