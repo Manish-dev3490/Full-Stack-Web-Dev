c419f51b541f85dbff6a707ef6c9a2b4000bdcf5
manish-dev3490 <kmanish87064@gmail.com>
2025-07-15 21:05:54 +0530

    info about React.createelement

---
+++ FrontEnd/React-in-Depth2/ReactElement/index.js
@@ -0,0 +1,9 @@
+// So in this file we are going to learn about what is React.CreateElement behind the scenes....
+
+
+
+// let us create a ReactElement ->so jese dom element hota hai wohi reactElement hota hai at the end of the day reactElement bhi dom me hee include hota hai so let us begin.
+
+const h2=React.createElement('h2',{className:'heading'},'Hello world through react element');
+console.log(h2);
+
