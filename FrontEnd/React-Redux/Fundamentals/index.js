366cdfbd7dc998be40d34a9f6a5ab9fb571a73ad
manish-dev3490 <kmanish87064@gmail.com>
2025-07-18 02:18:16 +0530

    bts of redux

--- FrontEnd/React-Redux/Fundamentals/index.js
+++ FrontEnd/React-Redux/Fundamentals/index.js
@@ -11,15 +11,26 @@ let initialState = {
     qualiication: "BCA",
 };
 
+
+const INCREMENT = 'count/increment';
+const DECREMENT = 'count/decrement';
+const DECREASE_BY = 'count/decrementBy';
+const INCREASE_BY = 'count/incrementBy';
+
+
+
+
+
+
 // Reducer is jese hamare store me states hoti hai unko hum directly modify nahi kar sakte unko update karne ke liye reducer function hote hai jinko use karke hum state ko change yah update kar sakte hai .Aur reducers function ke andar hum actions define karte jese humko jo kaam karvana hai. let me explian you breifly jese ki there is a global store and there are multiple state in that now i am using those states in my Appication and now we want to update tose state oon the event but we are not allowed to directly modify the states || we cannot mutate the state object to update the store states there is a feauture reducers reducers is nothing but functions are implemented in reducers which are used to update the states and those functions are called actions .those actions contains the type for verifyung.reducer have access to store and actions.
 function reducer(state = initialState, action) {
-    if (action.type === "count/increment") {
+    if (action.type === INCREMENT) {
         return { ...state, count: state.count + 1 };
-    } else if (action.type === "count/decrement") {
+    } else if (action.type === DECREMENT) {
         return { ...state, count: state.count - 1 };
-    } else if (action.type === "count/decrementBy") {
+    } else if (action.type === DECREASE_BY) {
         return { ...state, count: state.count - action.payload };
-    } else if (action.type === "count/incrementBy") {
+    } else if (action.type === INCREASE_BY) {
         return { ...state, count: state.count + action.payload };
     } else return state;
 }
@@ -37,9 +48,9 @@ Store.subscribe(function () {
 
 
 // Abh me chahta hu ki states ko update kru lekin woh kese hogi toh humko store ek function deta hai dispatch(action) iske andar humko action daalna padta hai aur dispatch store ke paas jaayega aur reducer ko call krega behind the scenes aur us action ke andar type,payload hoga reducer ke andar define hoga toh reducer usko call kar dega.let see in the example
-Store.dispatch({ type: 'count/increment', payload: undefined });
-Store.dispatch({ type: 'count/decrement', payload: undefined });
-Store.dispatch({ type: 'count/increment', payload: undefined });
-Store.dispatch({ type: 'count/increment', payload: undefined });
+Store.dispatch({ type: INCREMENT, payload: undefined });
+Store.dispatch({ type: DECREMENT, payload: undefined });
+Store.dispatch({ type: INCREASE_BY, payload: 10 });
+Store.dispatch({ type: DECREASE_BY, payload: 10 });
 
 
