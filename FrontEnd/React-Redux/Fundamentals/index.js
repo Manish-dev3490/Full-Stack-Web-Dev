49708080d65e650df4ea08b22e9083d809f1b7eb
manish-dev3490 <kmanish87064@gmail.com>
2025-07-18 01:49:51 +0530

    behind the scenes of the core redux and there functionality

--- FrontEnd/React-Redux/Fundamentals/index.js
+++ FrontEnd/React-Redux/Fundamentals/index.js
@@ -1,33 +1,18 @@
-// So in redux there is a global Store where we integrate our states so let us learn fundamentals step by step in the below code;
+// redux has deprecated the createStore now they offer redux toolkit configureStore function to setup a store but for learning purposes we can still use legacy createStore
+import { createStore } from "redux";
 
-// Assume this is the redux store and now we have to update its state without mutating(directly accessing the key values) of the object
-let Store = {
-  count: 0,
-};
-
-// So we will do by first copy the object using spread operator then now we have currentObject and we update we want
-function incrementCount(currentState) {
-  // we dont have to do this
-  // Store.count=Store.count+1;
-
-  // Not mutating we are actually changing the object
-  return { ...currentState, count: currentState.count + 1 };
-}
-
-// This incrementCount is returning us a new object with updated value and we are not even mutating our object
-Store = incrementCount(Store);
-
-// Now i am going to tell you how redux works behind the scenes assume kro yeh hamar belw store main redux store hai. SO redux kahta hai agar apko state update krni hai toh bina mutate kiye krni pdegi mutate ka matlab hai direct access krke nahi kar skte . toh without mutate toh ek hee tarika hai object ko copy kro aur usme change krke return krdo aur haan redux kahta hai apko state function ke through change krni pdegi jisko redux reducer bolta hai
-let reduxState = {
+// creating the initial state for using it in the reducer
+let initialState = {
   count: 0,
   name: "manish",
   age: 22,
+  conatct: 9643623490,
+  address: "359 chirag delhi",
+  qualiication: "BCA",
 };
 
-console.log(reduxState);
-
-// Abh yeh function jisko redux updater || reducer kahta hai termonology hai ek naya object return krega with update value aur copyied value ke saath usko hum reduxState me assign krdenge.uska naya refrence milte hee component ko re render kr deta hai this is how react works behin the scenes.
-function reducer(state, action) {
+// Reducer is jese hamare store me states hoti hai unko hum directly modify nahi kar sakte unko update karne ke liye reducer function hote hai jinko use karke hum state ko change yah update kar sakte hai .Aur reducers function ke andar hum actions define karte jese humko jo kaam karvana hai. let me explian you breifly jese ki there is a global store and there are multiple state in that now i am using those states in my Appication and now we want to update tose state oon the event but we are not allowed to directly modify the states || we cannot mutate the state object to update the store states there is a feauture reducers reducers is nothing but functions are implemented in reducers which are used to update the states and those functions are called actions .those actions contains the type for verifyung.reducer have access to store and actions.
+function reducer(state = initialState, action) {
   if (action.type === "count/increment") {
     return { ...state, count: state.count + 1 };
   } else if (action.type === "count/decrement") {
@@ -39,22 +24,11 @@ function reducer(state, action) {
   } else return state;
 }
 
-// Abh maanlo mujhe states me kuch update krna hai toh hum directly toh kar nahi sakte redux humko functionality deta hai reducers ki reducers are nothing but function which is used to update the state now we are going to write actions toh abh hamara reducer functions hai uske andar actions defined hai
+// createStore is a function which was used in the early days but now the people hve been migrated to the redux toolkit but before move to the redux toolkit let us learn the core redux . and nowdays we use configureStore() for creating the store but earlier we used createStore for creating the stiore as the name defines.So createStore function reducer ko leta hai reducer ko access hota hai initialState ka aur actions ka.
+const Store = createStore(reducer);
 
-reduxState = reducer(reduxState, { type: "count/increment" });
-console.log(reduxState);
 
-reduxState = reducer(reduxState, { type: "count/decrement" });
-console.log(reduxState);
+// Abh me chahta hu ki states ko update kru lekin woh kese hogi toh humko store ek function deta hai dispatch(action) iske andar humko action daalna padta hai aur dispatch store ke paas jaayega aur reducer ko call krega behind the scenes aur us action ke andar type,payload hoga reducer ke andar define hoga toh reducer usko call kar dega.let see in the example
 
-reduxState = reducer(reduxState, { type: "count/decrementBy", payload: 10 });
-console.log(reduxState);
-
-reduxState = reducer(reduxState, { type: "count/incrementBy", payload: 10 });
-console.log(reduxState);
-
-reduxState = reducer(reduxState, { type: "count/incrementBy", payload: 10 });
-console.log(reduxState);
-
-reduxState = reducer(reduxState, { type: "count/incrementBy", payload: 10 });
-console.log(reduxState);
+Store.dispatch({type:'count/increment',payload:undefined});
+console.log(Store.getState());
---
+++ FrontEnd/React-Redux/Fundamentals/reduxBTS.js
@@ -0,0 +1,65 @@
+// How redux works behind the scene is function me saara data with comments hai reduxer,action,updating state without mutate a object , actions and much more now we are going to learn actual redux
+function reduxBehindTheScenes() {
+    // So in redux there is a global Store where we integrate our states so let us learn fundamentals step by step in the below code;
+
+    // Assume this is the redux store and now we have to update its state without mutating(directly accessing the key values) of the object
+    let Store = {
+        count: 0,
+    };
+
+    // So we will do by first copy the object using spread operator then now we have currentObject and we update we want
+    function incrementCount(currentState) {
+        // we dont have to do this
+        // Store.count=Store.count+1;
+
+        // Not mutating we are actually changing the object
+        return { ...currentState, count: currentState.count + 1 };
+    }
+
+    // This incrementCount is returning us a new object with updated value and we are not even mutating our object
+    Store = incrementCount(Store);
+
+    // Now i am going to tell you how redux works behind the scenes assume kro yeh hamar belw store main redux store hai. SO redux kahta hai agar apko state update krni hai toh bina mutate kiye krni pdegi mutate ka matlab hai direct access krke nahi kar skte . toh without mutate toh ek hee tarika hai object ko copy kro aur usme change krke return krdo aur haan redux kahta hai apko state function ke through change krni pdegi jisko redux reducer bolta hai
+    let reduxState = {
+        count: 0,
+        name: "manish",
+        age: 22,
+    };
+
+    console.log(reduxState);
+
+    // Abh yeh function jisko redux updater || reducer kahta hai termonology hai ek naya object return krega with update value aur copyied value ke saath usko hum reduxState me assign krdenge.uska naya refrence milte hee component ko re render kr deta hai this is how react works behin the scenes.
+    function reducer(state, action) {
+        if (action.type === "count/increment") {
+            return { ...state, count: state.count + 1 };
+        } else if (action.type === "count/decrement") {
+            return { ...state, count: state.count - 1 };
+        } else if (action.type === "count/decrementBy") {
+            return { ...state, count: state.count - action.payload };
+        } else if (action.type === "count/incrementBy") {
+            return { ...state, count: state.count + action.payload };
+        } else return state;
+    }
+
+    // Abh maanlo mujhe states me kuch update krna hai toh hum directly toh kar nahi sakte redux humko functionality deta hai reducers ki reducers are nothing but function which is used to update the state now we are going to write actions toh abh hamara reducer functions hai uske andar actions defined hai
+
+    reduxState = reducer(reduxState, { type: "count/increment" });
+    console.log(reduxState);
+
+    reduxState = reducer(reduxState, { type: "count/decrement" });
+    console.log(reduxState);
+
+    reduxState = reducer(reduxState, { type: "count/decrementBy", payload: 10 });
+    console.log(reduxState);
+
+    reduxState = reducer(reduxState, { type: "count/incrementBy", payload: 10 });
+    console.log(reduxState);
+
+    reduxState = reducer(reduxState, { type: "count/incrementBy", payload: 10 });
+    console.log(reduxState);
+
+    reduxState = reducer(reduxState, { type: "count/incrementBy", payload: 10 });
+    console.log(reduxState);
+}
+
+export default reduxBehindTheScenes;
