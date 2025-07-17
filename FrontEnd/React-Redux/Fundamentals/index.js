788fe9c649d065a490326cbf224988ffd4c4dd72
manish-dev3490 <kmanish87064@gmail.com>
2025-07-18 02:05:54 +0530

    reducers,actions,store,dispatch understood all these termonlogy

--- FrontEnd/React-Redux/Fundamentals/index.js
+++ FrontEnd/React-Redux/Fundamentals/index.js
@@ -3,32 +3,43 @@ import { createStore } from "redux";
 
 // creating the initial state for using it in the reducer
 let initialState = {
-  count: 0,
-  name: "manish",
-  age: 22,
-  conatct: 9643623490,
-  address: "359 chirag delhi",
-  qualiication: "BCA",
+    count: 0,
+    name: "manish",
+    age: 22,
+    conatct: 9643623490,
+    address: "359 chirag delhi",
+    qualiication: "BCA",
 };
 
 // Reducer is jese hamare store me states hoti hai unko hum directly modify nahi kar sakte unko update karne ke liye reducer function hote hai jinko use karke hum state ko change yah update kar sakte hai .Aur reducers function ke andar hum actions define karte jese humko jo kaam karvana hai. let me explian you breifly jese ki there is a global store and there are multiple state in that now i am using those states in my Appication and now we want to update tose state oon the event but we are not allowed to directly modify the states || we cannot mutate the state object to update the store states there is a feauture reducers reducers is nothing but functions are implemented in reducers which are used to update the states and those functions are called actions .those actions contains the type for verifyung.reducer have access to store and actions.
 function reducer(state = initialState, action) {
-  if (action.type === "count/increment") {
-    return { ...state, count: state.count + 1 };
-  } else if (action.type === "count/decrement") {
-    return { ...state, count: state.count - 1 };
-  } else if (action.type === "count/decrementBy") {
-    return { ...state, count: state.count - action.payload };
-  } else if (action.type === "count/incrementBy") {
-    return { ...state, count: state.count + action.payload };
-  } else return state;
+    if (action.type === "count/increment") {
+        return { ...state, count: state.count + 1 };
+    } else if (action.type === "count/decrement") {
+        return { ...state, count: state.count - 1 };
+    } else if (action.type === "count/decrementBy") {
+        return { ...state, count: state.count - action.payload };
+    } else if (action.type === "count/incrementBy") {
+        return { ...state, count: state.count + action.payload };
+    } else return state;
 }
 
 // createStore is a function which was used in the early days but now the people hve been migrated to the redux toolkit but before move to the redux toolkit let us learn the core redux . and nowdays we use configureStore() for creating the store but earlier we used createStore for creating the stiore as the name defines.So createStore function reducer ko leta hai reducer ko access hota hai initialState ka aur actions ka.
 const Store = createStore(reducer);
 
 
-// Abh me chahta hu ki states ko update kru lekin woh kese hogi toh humko store ek function deta hai dispatch(action) iske andar humko action daalna padta hai aur dispatch store ke paas jaayega aur reducer ko call krega behind the scenes aur us action ke andar type,payload hoga reducer ke andar define hoga toh reducer usko call kar dega.let see in the example
+// Abh jese hee state update hori hai toh humko store.getState() karke dekhni padd rhi hai uske liye bhi ek function hai subscribe
+Store.subscribe(function () {
+    console.log(Store.getState());
 
-Store.dispatch({type:'count/increment',payload:undefined});
-console.log(Store.getState());
+})
+
+
+
+// Abh me chahta hu ki states ko update kru lekin woh kese hogi toh humko store ek function deta hai dispatch(action) iske andar humko action daalna padta hai aur dispatch store ke paas jaayega aur reducer ko call krega behind the scenes aur us action ke andar type,payload hoga reducer ke andar define hoga toh reducer usko call kar dega.let see in the example
+Store.dispatch({ type: 'count/increment', payload: undefined });
+Store.dispatch({ type: 'count/decrement', payload: undefined });
+Store.dispatch({ type: 'count/increment', payload: undefined });
+Store.dispatch({ type: 'count/increment', payload: undefined });
+
+
