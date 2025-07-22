14545247bc92a5fa781f957781d52168c51b0154
manish-dev3490 <kmanish87064@gmail.com>
2025-07-23 02:43:39 +0530

    improved the structure of the files

--- FrontEnd/React-Redux/MultipleReducers/index.js
+++ FrontEnd/React-Redux/MultipleReducers/index.js
@@ -8,9 +8,12 @@ let initialState = {
   wishList: [],
 };
 
-// creating the reducer
-function reducer(state = initialState, action) {}
+// creating the reducer for passing it to the store
+function reducer(state = initialState, action) {
+  console.log(action);
+  return state;
+}
 
 // creating the global redux store --> Store takes two parameters first one is reducer and second one for connecting react with redux dev tools
 const Store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__?.());
-console.log(Store);
+Store.dispatch({type:'cart/addItems',payload:{productID:1,quantity:1}});
