027860b4a6ebd0ae45ed3e78020479fa37a9dcf2
manish-dev3490 <kmanish87064@gmail.com>
2025-07-23 02:19:22 +0530

    done some changes in the files

--- FrontEnd/React-Redux/MultipleReducers/index.js
+++ FrontEnd/React-Redux/MultipleReducers/index.js
@@ -1,7 +1,7 @@
 import productList from "./productList";
 import { createStore } from "redux";
 
-// creating the initial state
+// creating the initial state for the reducer
 let initialState = {
   products: productList,
   cartItems: [],
@@ -11,5 +11,6 @@ let initialState = {
 // creating the reducer
 function reducer(state = initialState, action) {}
 
-// creating the global redux store
-const Store = createStore(reducer);
+// creating the global redux store --> Store takes two parameters first one is reducer and second one for connecting react with redux dev tools
+const Store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__?.());
+console.log(Store);
