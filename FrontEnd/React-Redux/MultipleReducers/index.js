0a3143f639f4b5c45a2fe7da71c6efd936023b90
manish-dev3490 <kmanish87064@gmail.com>
2025-07-23 22:02:24 +0530

    managing some complex states

--- FrontEnd/React-Redux/MultipleReducers/index.js
+++ FrontEnd/React-Redux/MultipleReducers/index.js
@@ -10,10 +10,13 @@ let initialState = {
 
 // creating the reducer for passing it to the store
 function reducer(state = initialState, action) {
-  console.log(action);
+  if(action.type=='cart/addItems'){
+    return {...state,cartItems:[...state.cartItems,action.payload]}
+  }
   return state;
 }
 
 // creating the global redux store --> Store takes two parameters first one is reducer and second one for connecting react with redux dev tools
 const Store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__?.());
 Store.dispatch({type:'cart/addItems',payload:{productID:1,quantity:1}});
+console.log(Store.getState()); 
