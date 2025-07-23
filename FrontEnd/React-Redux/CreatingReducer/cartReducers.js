ffc2898e10269287afe5c9a609ba8229d9c6bf57
manish-dev3490 <kmanish87064@gmail.com>
2025-07-24 02:00:32 +0530

    implmeneted the combine reducers function propelry

--- FrontEnd/React-Redux/CreatingReducer/cartReducers.js
+++ FrontEnd/React-Redux/CreatingReducer/cartReducers.js
@@ -7,37 +7,38 @@ export const cart_Decrease_Quantity = "cart/DecreaseQuantity";
 // Reducer using if-else
 export default function cartReducer(state = [], action) {
 
-  if (action.type === cart_Add_Item) {
-    return [...state, action.payload];
-  }
+    if (action.type === cart_Add_Item) {
+        return [...state, action.payload];
+    }
 
-  else if (action.type === cart_Remove_Item) {
-    return state.filter(item => item.productID !== action.payload.productID);
-  }
+    else if (action.type === cart_Remove_Item) {
+        return state.filter(item => item.productID !== action.payload.productID);
+    }
 
-  else if (action.type === cart_Increase_Quantity) {
-    return state.map(item => {
-      if (item.productID === action.payload.productID) {
-        return {
-          ...item,
-          quantity: item.quantity + 1
-        };
-      }
-      return item;
-    });
-  }
 
-  else if (action.type === cart_Decrease_Quantity) {
-    return state.map(item => {
-      if (item.productID === action.payload.productID && item.quantity > 1) {
-        return {
-          ...item,
-          quantity: item.quantity - 1
-        };
-      }
-      return item;
-    });
-  }
+    else if (action.type === cart_Increase_Quantity) {
+        return state.map(item => {
+            if (item.productID === action.payload.productID) {
+                return {
+                    ...item,
+                    quantity: item.quantity + 1
+                };
+            }
+            return item;
+        });
+    }
 
-  return state;
+    else if (action.type === cart_Decrease_Quantity) {
+        return state.map(item => {
+            if (item.productID === action.payload.productID && item.quantity > 1) {
+                return {
+                    ...item,
+                    quantity: item.quantity - 1
+                };
+            }
+            return item;
+        });
+    }
+
+    return state;
 }
