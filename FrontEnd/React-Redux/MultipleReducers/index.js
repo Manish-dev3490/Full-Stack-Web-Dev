a498da8b968d10e7b0a00c03e85a4b0427473a72
manish-dev3490 <kmanish87064@gmail.com>
2025-07-23 23:00:06 +0530

    learning about the flow of redux library

--- FrontEnd/React-Redux/MultipleReducers/index.js
+++ FrontEnd/React-Redux/MultipleReducers/index.js
@@ -10,24 +10,86 @@ let initialState = {
 
 // creating the reducer for passing it to the store
 function reducer(state = initialState, action) {
-  if(action.type=='cart/addItems'){
-    return {...state,cartItems:[...state.cartItems,action.payload]}
+  // handling condition for adding items from cart
+  if (action.type == "cart/addItems") {
+    return { ...state, cartItems: [...state.cartItems, action.payload] };
   }
 
-  else if(action.type=='cart/removeItems'){
-    console.log(action.payload);
-    
-    return {...state,cartItems:state.cartItems.filter((cart)=>{return cart.productID!==action.payload.productID})}
+  // handling condition for removing items from cart
+  else if (action.type == "cart/removeItems") {
+    return {
+      ...state,
+      cartItems: state.cartItems.filter((cart) => {
+        return cart.productID !== action.payload.productID;
+      }),
+    };
+  }
+
+  // handling condition for increasingItemQuantity
+  else if (action.type == "cart/increaseItemQuantity") {
+    return {
+      ...state,
+      cartItems: state.cartItems.map((cart) => {
+        if (cart.productID === action.payload.productID) {
+          return { ...cart, quantity: cart.quantity + 1 };
+        }
+        return cart; // very important to return cart if no match
+      }),
+    };
+  }
+
+
+
+  // handling condition for decreasingItemQuantity
+  else if (action.type == "cart/decreaseItemQuantity") {
+    return {
+      ...state,
+      cartItems: state.cartItems.map((cart) => {
+        if (cart.productID === action.payload.productID) {
+          return { ...cart, quantity: cart.quantity - 1 };
+        }
+        return cart; // very important to return cart if no match
+      }),
+    };
   }
   return state;
 }
 
 // creating the global redux store --> Store takes two parameters first one is reducer and second one for connecting react with redux dev tools
-const Store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__?.());
-Store.dispatch({type:'cart/addItems',payload:{productID:1,quantity:1}});
-Store.dispatch({type:'cart/addItems',payload:{productID:2,quantity:1}});
-Store.dispatch({type:'cart/addItems',payload:{productID:3,quantity:1}});
-Store.dispatch({type:'cart/removeItems',payload:{productID:3}});
+const Store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
+Store.dispatch({
+  type: "cart/addItems",
+  payload: { productID: 1, quantity: 1 },
+});
+Store.dispatch({
+  type: "cart/addItems",
+  payload: { productID: 2, quantity: 1 },
+});
+Store.dispatch({
+  type: "cart/addItems",
+  payload: { productID: 3, quantity: 1 },
+});
+Store.dispatch({ type: "cart/removeItems", payload: { productID: 3 } });
+Store.dispatch({
+  type: "cart/increaseItemQuantity",
+  payload: { productID: 2 },
+});
+Store.dispatch({
+  type: "cart/increaseItemQuantity",
+  payload: { productID: 2 },
+});
+Store.dispatch({
+  type: "cart/increaseItemQuantity",
+  payload: { productID: 2 },
+});
+Store.dispatch({
+  type: "cart/increaseItemQuantity",
+  payload: { productID: 2 },
+});
 
 
-console.log(Store.getState()); 
+Store.dispatch({
+  type: "cart/decreaseItemQuantity",
+  payload: { productID: 2 },
+});
+
