51c7e7d3651d1418dbd7bbcc6441050d6445dec0
manish-dev3490 <kmanish87064@gmail.com>
2025-08-22 01:40:30 +0530

    type conversion is done

--- FrontEnd/JS-in-Depth/Basics/TypeCoersion.js
+++ FrontEnd/JS-in-Depth/Basics/TypeCoersion.js
@@ -1,2 +1,18 @@
-// Now here i will learn type coersion in very very very depth
+// Type conversion - is a process in which we explicitly do datatype change into other like string ko number me yah fhir number ko string me toh functiona aur opertaor ke thorugh krna chahiye recommended way hai.
+
+// Number ko string me krdo using using String()
+let num=123;
+num=String(num);
+
+// string ko number me kardo 
+let name="manish";
+name=Number(name);
+
+// Number to boolean
+let age=22;
+age=Boolean(age);
+
+// boolean ko number me 
+let value=true;
+value=Number(value);
 
