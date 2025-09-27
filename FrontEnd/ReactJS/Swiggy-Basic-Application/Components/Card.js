51586df67fcb4049bbadbdf1d592ddbd16e17263
manish-dev3490 <kmanish87064@gmail.com>
2025-09-27 21:03:01 +0530

    done some styling and understood configre driven UI

--- FrontEnd/ReactJS/Swiggy-Basic-Application/Components/Card.js
+++ FrontEnd/ReactJS/Swiggy-Basic-Application/Components/Card.js
@@ -1,12 +1,15 @@
 // This is the card component for our application 
-const Card=()=> {
+const Card = () => {
   return (
     <div className="res-card">
-        <img src="https://images7.alphacoders.com/596/596343.jpg" alt="pizza"/>
+      <img src="https://images7.alphacoders.com/596/596343.jpg" alt="pizza" />
+      <div className="card-desc">
         <h3>Title of the resaturent</h3>
         <h4>ratings</h4>
         <p>cuisnes Data</p>
         <p>Delivery Time  30 minutes</p>
+      </div>
+
     </div>
   )
 }
