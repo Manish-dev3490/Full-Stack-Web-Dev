6f56816a9de979faeba7a23deab68dceed04ada6
manish-dev3490 <kmanish87064@gmail.com>
2025-09-28 18:22:27 +0530

    deleted application swiggy which was created with parcel

--- FrontEnd/ReactJS/Swiggy-Basic-Application/Components/Footer.js
+++
@@ -1,8 +0,0 @@
-// This is the footer level component for our application
-const Footer = () => {
-  return (
-    <div className="footer">Copyright reserved to the application of food delivery for the project</div>
-  )
-}
-
-export default Footer
