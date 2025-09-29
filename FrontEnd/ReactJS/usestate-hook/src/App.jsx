ce9e3f9a7d136ad0ea6e857bb4692e07f60dd5fd
manish-dev3490 <kmanish87064@gmail.com>
2025-09-30 01:02:48 +0530

    created a new react project for learning useState hook in the depth

---
+++ FrontEnd/ReactJS/usestate-hook/src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+  let count=0;
+  return (
+    <>
+   <div className="app-component">
+    <button className="increase">Increase</button>
+    <h2>{count}</h2>
+    <button className="decrease">decrease</button>
+
+   </div>
+      
+    </>
+  )
+}
+
+export default App
