a4423e3180d99b12384e06a358ef1cf731e51795
manish-dev3490 <kmanish87064@gmail.com>
2025-09-30 13:48:16 +0530

    i will do it aftre some minutes

--- FrontEnd/ReactJS/usestate-hook/src/App.jsx
+++ FrontEnd/ReactJS/usestate-hook/src/App.jsx
@@ -1,3 +1,4 @@
+import ReactDOM from 'react-dom/client';
 
 function App() {
 
@@ -5,8 +6,13 @@ function App() {
   return (
     <>
    <div className="app-component">
-    <button className="increase">Increase</button>
-    <h2>{count}</h2>
+    <button className="increase" onClick={()=>{
+      console.log(count++);
+      
+      const root=ReactDOM.createRoot(document.querySelector(".heading"));
+
+    }}>Increase</button>
+    <h2 className="heading">{count}</h2>
     <button className="decrease">decrease</button>
 
    </div>
