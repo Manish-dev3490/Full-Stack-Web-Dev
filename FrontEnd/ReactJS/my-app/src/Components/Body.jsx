eeaf16187054b01d62750a314d2b16af016a3e64
manish-dev3490 <kmanish87064@gmail.com>
2025-10-02 14:58:34 +0530

    implemented conditional rendering into our applications

--- FrontEnd/ReactJS/my-app/src/Components/Body.jsx
+++ FrontEnd/ReactJS/my-app/src/Components/Body.jsx
@@ -2,42 +2,45 @@ import { useState } from "react";
 import Card from "./Card";
 import FilterRastaurants from "./FilterRastaurants";
 import { useEffect } from "react";
-import fetchData from "../hooks/fetchData";
+import { swiggyApi } from "../utils/constData";
 
+// This is the Body level componnent for our application
 const Body = () => {
+  // state variables for our compnents
   const [value, setValue] = useState([]);
-  const [loading, setLoading] = useState(true);
 
+  // iske andar ka callback function will run after component will render
   useEffect(() => {
-    const getData = async () => {
-      const data = await fetchData();
-      setValue(data);
-      setLoading(false);
-    };
-    getData();
+    fetchData();
   }, []);
+
+  const fetchData = async function () {
+    const data = await fetch(swiggyApi);
+    const res = await data.json();
+    const restaurants = res.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
+    setValue(restaurants);
+  };
 
   return (
     <div className="body-container">
       <div className="search-bar">
-        <input className="search-area" type="text" placeholder="search your favourite restaurent" />
+        <input
+          className="search-area"
+          type="text"
+          placeholder="search your favourite restaurent"
+        />
         <button className="srch-btn">Search</button>
         <FilterRastaurants value={value} setValue={setValue} />
       </div>
-
-     <div className="res-container">
-        {loading ? (
-          <h2>Loading restaurants...</h2>
-        ) : value.length === 0 ? (
-          <h2>No restaurants found</h2>
+      <div className="res-container">
+        {value.length === 0 ? (
+          <h1>Data is loading...</h1>
         ) : (
-          value.map((resCard) => (
-            <Card key={resCard.info.id} resData={resCard.info} />
-          ))
+          value.map((resCard) => <Card key={resCard.info.id} resData={resCard.info} />
+          )
         )}
       </div>
-
     </div>
-  )
-}
-export default Body
+  );
+};
+export default Body;
