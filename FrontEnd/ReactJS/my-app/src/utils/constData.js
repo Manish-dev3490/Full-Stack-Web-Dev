92fe8b924e3c4aa3498df513f31f167df9919105
manish-dev3490 <kmanish87064@gmail.com>
2025-10-02 22:42:41 +0530

    changes something

--- FrontEnd/ReactJS/my-app/src/utils/constData.js
+++ FrontEnd/ReactJS/my-app/src/utils/constData.js
@@ -2,3 +2,4 @@ const headerLogo="https://logos-world.net/wp-content/uploads/2023/05/Food-Networ
 const imgURL='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/'
 const swiggyApi="https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.63270&lng=77.21980&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 export {headerLogo,imgURL,swiggyApi}
+
