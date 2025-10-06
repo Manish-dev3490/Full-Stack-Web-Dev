09d9ecc97e7d206d50a035c714e2671f8c728437
manish-dev3490 <kmanish87064@gmail.com>
2025-10-06 14:44:46 +0530

    implemented lazy loading code splitting and chunking in my application to make it optimized

--- FrontEnd/ReactJS/my-app/src/Components/App.jsx
+++ FrontEnd/ReactJS/my-app/src/Components/App.jsx
@@ -2,39 +2,44 @@ import Header from "./Header";
 import Body from "./Body";
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
-import About from "./About";
 import Cart from "./Cart";
 import NotFound from "./NotFound";
 import Detail from "./Detail";
 import Footer from "./Footer";
-import Contact from "./Contact"
+import Contact from "./Contact";
+import { Suspense, lazy } from "react";
 
+const About = lazy(() => import("./About")); // ✅ fixed
 
-// This is our App level component all the other sub component will be inside it 
+// App Component
 const App = () => {
-    return (
-        <div className="App-Comp">
-            <Header />
-            <Body />
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="App-Comp">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  )
 }
 
 createRoot(document.getElementById('root')).render(
-   
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App></App>}></Route>
-                <Route path="*" element={<NotFound></NotFound>}></Route>
-                <Route path="/Detail/:id" element={<Detail></Detail>}></Route>
-                <Route path="/About" element={<About></About>}></Route>
-                <Route path="/Contact" element={<Contact></Contact>}></Route>
-                <Route path="/Cart" element={<Cart></Cart>}></Route>
-            </Routes>
-        </BrowserRouter>
-    
-)
-
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/Detail/:id" element={<Detail />} />
+      <Route
+        path="/About"
+        element={
+          <Suspense fallback={<h2>Loading...</h2>}>
+            <About />
+          </Suspense>
+        }
+      />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="/Cart" element={<Cart />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
