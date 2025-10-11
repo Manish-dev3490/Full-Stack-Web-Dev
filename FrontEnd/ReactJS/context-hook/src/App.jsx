0fe3c04fc924273c6cb7a3a9d8a1a2815ef8adfa
manish-dev3490 <kmanish87064@gmail.com>
2025-10-11 14:14:36 +0530

    created a new react project for understanding th context api

---
+++ FrontEnd/ReactJS/context-hook/src/App.jsx
@@ -0,0 +1,17 @@
+import First from "./components/First"
+import Second from "./components/Second"
+import Third from "./components/Third"
+
+
+function App() {
+
+  return (
+    <>
+     <First/>
+     <Second/>
+     <Third/>
+    </>
+  )
+}
+
+export default App
