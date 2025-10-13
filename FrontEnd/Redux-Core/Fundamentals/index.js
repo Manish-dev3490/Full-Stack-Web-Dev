f2688d8d9cb06a02b292f75d2e27a2206168b985
manish-dev3490 <kmanish87064@gmail.com>
2025-10-13 16:54:40 +0530

    implemeneted reducer actions on my wn behind the scenes of the redux

--- FrontEnd/Redux-Core/Fundamentals/index.js
+++ FrontEnd/Redux-Core/Fundamentals/index.js
@@ -23,8 +23,6 @@ function howReduxwantsUsToMutateObject() {
 
 // This is the mechanism of redux behind the scenes reducer stre state action and many more thing 
 function reduxMechanism(){
-
-
 // Assume kro yeh hamare react app ki puri saari states hai ek store me inko update krne ke liye ek helper function chahiye which is called reducer in redux. 
 let reduxState={
     count:0,
@@ -32,12 +30,13 @@ let reduxState={
     name:'manish'
 }
 
-
 // reducer- reducer is a helper function which is used to take 2 arguments state of the application and action jisse yeh pata chlega puri app ki state me kis state ko change krna hai aur kya change krna hai.
 
 function reducer(reduxState,action){
-if(action.type==='increase/count')return {...reduxState,count:reduxState.count+1};
-else if(action.type==='decrease/count')return {...reduxState,count:reduxState.count-1};
+if(action.type==='count/increase')return {...reduxState,count:reduxState.count+1};
+else if(action.type==='count/decrease')return {...reduxState,count:reduxState.count-1};
+else if(action.type==='count/decreaseBy')return {...reduxState,count:reduxState.count-action.payload};
+else if(action.type==='count/increaseBy')return {...reduxState,count:reduxState.count+action.payload};
 else return reduxState; 
 }
 
@@ -45,13 +44,14 @@ else return reduxState;
 
 // Abh redux kisi event par jab aplication se msg aata hai ki state change krni hai call krti hai reducer ko state deti hai aur action paas krti hai reducer function obviosuly woh action redux ko developer se milte hai
 
-reduxState=reducer(reduxState,{type:'increase/count',payload:undefined});
+reduxState=reducer(reduxState,{type:'count/increase',payload:undefined});
 console.log(reduxState);
-reduxState=reducer(reduxState,{type:'decrease/count',payload:undefined});
+reduxState=reducer(reduxState,{type:'count/decrease',payload:undefined});
 console.log(reduxState);
-
+reduxState=reducer(reduxState,{type:'count/increaseBy',payload:50});
+console.log(reduxState);
 }
 
 
-// reduxMechanism()
+reduxMechanism()
 
