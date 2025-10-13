ddcd3707a6e63e6bbfa61e99526e436e633d16d6
manish-dev3490 <kmanish87064@gmail.com>
2025-10-13 20:17:03 +0530

    understood the whole ecosystem of redux core

--- FrontEnd/Redux-Core/Fundamentals/index.js
+++ FrontEnd/Redux-Core/Fundamentals/index.js
@@ -1,28 +1,6 @@
-// This is how we mutate the object which is wrong
-function howweMutateObject() {
-    // this is how we genrally mutate the object but in redux asa directly mutate nahi krte 
-    let state = {
-        count: 0
-    }
-    state.count = state.count + 1;
-}
+import { legacy_createStore } from "redux";
 
 
-// This is the recommended way to mutate object in react redux
-function howReduxwantsUsToMutateObject() {
-    // This is how we mutate object in redux nayi state return krte hai with updated value
-    function increment(state) {
-        const newObj = { ...state, count: state.count + 1 }
-        return newObj;
-    }
-
-    const obj = increment(state);
-    console.log(obj);
-}
-
-
-// This is the mechanism of redux behind the scenes reducer stre state action and many more thing 
-function reduxMechanism(){
 // Assume kro yeh hamare react app ki puri saari states hai ek store me inko update krne ke liye ek helper function chahiye which is called reducer in redux. 
 let reduxState={
     count:0,
@@ -32,25 +10,21 @@ let reduxState={
 
 // reducer- reducer is a helper function which is used to take 2 arguments state of the application and action jisse yeh pata chlega puri app ki state me kis state ko change krna hai aur kya change krna hai.
 
-function reducer(reduxState,action){
-if(action.type==='count/increase')return {...reduxState,count:reduxState.count+1};
-else if(action.type==='count/decrease')return {...reduxState,count:reduxState.count-1};
-else if(action.type==='count/decreaseBy')return {...reduxState,count:reduxState.count-action.payload};
-else if(action.type==='count/increaseBy')return {...reduxState,count:reduxState.count+action.payload};
+function reducer(state=reduxState,action){
+if(action.type==='count/increase')return {...state,count:state.count+1};
+else if(action.type==='count/decrease')return {...state,count:state.count-1};
+else if(action.type==='count/decreaseBy')return {...state,count:state.count-action.payload};
+else if(action.type==='count/increaseBy')return {...state,count:state.count+action.payload};
 else return reduxState; 
 }
 
 
 
-// Abh redux kisi event par jab aplication se msg aata hai ki state change krni hai call krti hai reducer ko state deti hai aur action paas krti hai reducer function obviosuly woh action redux ko developer se milte hai
+const store=legacy_createStore(reducer);
+store.subscribe(()=>{
+    console.log(store.getState());
+})
 
-reduxState=reducer(reduxState,{type:'count/increase',payload:undefined});
-console.log(reduxState);
-reduxState=reducer(reduxState,{type:'count/decrease',payload:undefined});
-console.log(reduxState);
-reduxState=reducer(reduxState,{type:'count/increaseBy',payload:50});
-console.log(reduxState);
-}
-
-reduxMechanism()
-
+store.dispatch({type:'count/increase',payload:undefined});
+store.dispatch({type:'count/increase',payload:undefined});
+store.dispatch({type:'count/increase',payload:undefined});
