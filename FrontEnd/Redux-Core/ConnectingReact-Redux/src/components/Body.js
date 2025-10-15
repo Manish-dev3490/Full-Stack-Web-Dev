b3be2041a3575ebdce169d1b76e800252fc7039d
manish-dev3490 <kmanish87064@gmail.com>
2025-10-15 18:29:17 +0530

    now we have connected our react app with redux store

--- FrontEnd/Redux-Core/ConnectingReact-Redux/src/components/Body.js
+++ FrontEnd/Redux-Core/ConnectingReact-Redux/src/components/Body.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Card from "./Card";
+import {useSelector} from "react-redux"
 const Body = () => {
+  const countValue=useSelector((state)=>state?.count);
+  console.log(countValue);
+  
+
   return (
     <main
       style={{
