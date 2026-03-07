1336915c3cbe06ab13c6299e6f85243c83e728f2
manish-dev3490 <kmanish87064@gmail.com>
2026-03-07 22:48:06 +0530

    understood the core of react and reactDOm and implemented mini react

--- FrontEnd/React19/fundamentals/introduction/ourCustomReact.js
+++ FrontEnd/React19/fundamentals/introduction/ourCustomReact.js
@@ -68,25 +68,39 @@ function whyWeNeedReactReasons() {
 
 // Now let me tel you about the React Introduction->So React is nothing but just a js library(menaning of library is the predefined code we have to only use likhne ki jrurt nahi just like in c++ stl we dont have to create stack,queue,priorityqueue,map,set,sort function all are predefined).so react is also just a library buss isko use krna hai humko ek object milta hai React naam ka jo banaya hai facebook meta ke developers ne productvity increase krne ke liye baar baar dry principle naah ho and aur bhi bahut reasons hai sab dekehege and react ek object hai usme diffrent multiple functions methods and jinki alag alag functionalities hai aur kuch nahi.
 
-// const React = {
-//   createElement: function (tag, attributes, children) {
-//     // This is for creating element
-//     const element = document.createElement(tag);
-//     element.textContent = children;
-//     // lets put attriibutes in that element like class, id, style so jo attributes honge woh ek object aaygea because multiple values aayege toh ek object me wrap up hokar aayegi toh hum ek ek karke toh daalege nahi object par iterate krna pdega
+const React = {
+  createElement: function (tag, attributes, children) {
+    return {
+      type: tag,
+      props: {
+        ...attributes,
+        children,
+      },
+      ref: null,
+      owner: null,
+      store: null,
+    };
+  },
+  createRoot: function () { },
+};
 
-//     for (key in attributes) {
-//       if (key == "style") {
-//         Object.assign(element.style, attributes.style);
-//       } else element[key] = attributes[key];
-//     }
+const ReactDOM = {
+  render: function (element, root) {
+    const { props } = element;
+    console.log(props);
 
-//     return element;
-//   },
-//   createRoot: function () {},
-// };
+    const elem = document.createElement(props.tag);
+    for (const key in props) {
+      if (key === "style") {
+        Object.assign(elem.style, props.style);
+      } else if (key === "children") {
+        elem.textContent = props.children;
+      } else elem.key = props.key;
+    }
+    root.append(elem);
+  },
+};
+const elem1 = React.createElement("h1", { class: "elem", id: "element", style: { fontSize: "20px", color: "red", backgroundColor: "black" } }, "Hello baccho kese ho aap");
 
-const elem1 = React.createElement("h1", {}, "Hello baccho kese ho aap");
 const root = document.querySelector(".root");
-ReactDOM.render(elem1,root);
-
+ReactDOM.render(elem1, root);
