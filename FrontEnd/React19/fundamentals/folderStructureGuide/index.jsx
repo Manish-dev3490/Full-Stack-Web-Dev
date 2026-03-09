e8092657a13c76153e6ead78285fd94bae3e8a30
manish-dev3490 <kmanish87064@gmail.com>
2026-03-09 21:27:56 +0530

    created the basic react application

---
+++ FrontEnd/React19/fundamentals/folderStructureGuide/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const elem1=<h1>Hello World</h1>;
+const root=ReactDOM.createRoot(document.querySelector(".root"));
+root.render(elem1);
