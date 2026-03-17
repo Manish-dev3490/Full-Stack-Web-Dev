288a1864b7affee56393d236aefc2966e34a7574
manish-dev3490 <kmanish87064@gmail.com>
2026-03-17 22:11:11 +0530

    implemented form using zod

--- FrontEnd/React19/fundamentals/form-hook/src/Form.jsx
+++ FrontEnd/React19/fundamentals/form-hook/src/Form.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from "zod"
+
+const formSchema = z.object({
+    name: z.string().min(5, "Minimumlength should be 5").max(20, "maximum length should be 20"),
+    email: z.string().min(5, "Minimumlength should be 5").max(20, "maximum length should be 20"),
+    password: z.string().min(5, "Minimumlength should be 5").max(20, "maximum length should be 20"),
+})
 
 export function Form() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: zodResolver(formSchema)
+    });
 
     function handleForm(data) {
         console.log(data);
     }
-    console.log("hello world");
 
     return (
         <>
@@ -15,23 +24,26 @@ export function Form() {
                 <div>
                     <label htmlFor='first'>Name :</label>
                     <input id='first' type='text' {...register('name')} />
-
+                    {errors.name && <span>{errors.name.message}</span>}
                 </div>
 
                 <div>
                     <label htmlFor='second'>Age :</label>
                     <input id='second' type='text' {...register('age')} />
+                    {errors.age && <span>{errors.age.message}</span>}
                 </div>
 
                 <div>
                     <label htmlFor='third'>Email::</label>
                     <input id='third' type='text' {...register('email')} />
+                    {errors.email && <span>{errors.email.message}</span>}
                 </div>
 
 
                 <div>
                     <label htmlFor='fourth'>Password :</label>
                     <input id='fourth' type='text' {...register('password')} />
+                    {errors.password && <span>{errors.password.message}</span>}
                 </div>
                 <button type='submit'>Submit</button>
             </form>
