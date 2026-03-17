668b5b407276575a9eb239b89b8194479cabee52
manish-dev3490 <kmanish87064@gmail.com>
2026-03-17 18:41:55 +0530

    understanding react-form hok

---
+++ FrontEnd/React19/fundamentals/form-hook/src/Form.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {useForm} from 'react-hook-form';
+
+ export function Form() {
+    const { register, handleSubmit } = useForm();
+    
+    function handleForm(data) {
+        console.log(data);
+    }
+    console.log("hello world");
+    
+    return (
+        <>
+            <form onSubmit={handleSubmit(handleForm)} >
+                <div>
+                    <label htmlFor='first'>Name :</label>
+                    <input id='first' type='text' {...register('name')} />
+                </div>
+
+                <div>
+                    <label htmlFor='second'>Age :</label>
+                    <input id='second' type='text' {...register('age')} />
+                </div>
+
+                <div>
+                    <label htmlFor='third'>Email::</label>
+                    <input id='third' type='text' {...register('email')} />
+                </div>
+
+
+                <div>
+                    <label htmlFor='fourth'>Password :</label>
+                    <input id='fourth' type='text' {...register('password')} />
+                </div>
+                <button type='submit'>Submit</button>
+            </form>
+
+        </>
+    )
+}
