5157558827a55903203f7d635fc20b865bba6a2a
manish-dev3490 <kmanish87064@gmail.com>
2026-04-01 21:23:44 +0530

    we have done with the structures of the components

---
+++ FrontEnd/redux-toolkit/src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Counter() {
+  return (
+   <>
+   <p>Value of the counbter is : {}</p>
+    <button>Increment</button>
+    <button>Decrement</button>
+    <button>Reset</button>
+
+
+   </>
+  )
+}
+
+export default Counter
