40f77805b851e6940547cd4d66e73224ff0e2cb2
manish-dev3490 <kmanish87064@gmail.com>
2026-04-17 13:24:03 +0530

    understod the things in express

--- Backend/express-js/index.js
+++ Backend/express-js/index.js
@@ -1,3 +1,25 @@
 const express=require('express');
-console.log(express());
 
+const app=express();
+app.listen(4000,()=>{
+    console.log("i am listening the request");
+})
+
+
+app.use("/about",(req,res)=>{
+    res.send("hello ai am about")
+
+})
+
+app.use("/contact/:id",(req,res)=>{
+    console.log(req.params);
+    
+    res.send("hello ai am contact")
+
+})
+
+
+app.use("/",(req,res)=>{
+    res.send("hello ai am expreess")
+    
+})
