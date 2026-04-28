e3ff5379d9a36ed93001fdcb62ef6c21de73b222
manish-dev3490 <kmanish87064@gmail.com>
2026-04-28 14:31:12 +0530

    changes the dbname

--- Backend/MongoDB/database.js
+++ Backend/MongoDB/database.js
@@ -6,12 +6,16 @@ async function runGetStarted() {
     // Replace the uri string with your connection string
     const uri = 'mongodb+srv://kmanish87064_db_user:CodingAdda123@coding-adda.neh8o7x.mongodb.net/';
     const client = new MongoClient(uri);
-    const dbname = 'LearnDB';
+    const dbname = 'devCircle-data';
 
 
     await client.connect();
     const db = client.db(dbname);
    const collection=db.collection('user');
+
+   const findalldocs=await collection.find({}).toArray();
+   console.log("Found all the documents ",findalldocs);
+   
     console.log("we are connected with db");
 
 }
