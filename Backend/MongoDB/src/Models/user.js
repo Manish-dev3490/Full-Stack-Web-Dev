eec8cfabb7506d87c2cb13347b0960ff94c6fd93
manish-dev3490 <kmanish87064@gmail.com>
2026-05-01 20:42:02 +0530

    created some route handlers

--- Backend/MongoDB/src/Models/user.js
+++ Backend/MongoDB/src/Models/user.js
@@ -1,12 +1,40 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    age: Number,
-    email: String,
-    password: String
-});
+    firstName: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 30,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        minLength: 3,
+        maxLength: 30,
+        trim: true
+    },
+    age: {
+        type: Number,
+        min: 18,
+        max: 60
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"]
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        maxLength: 25,
+        trim: true
+    }
+}, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- Backend/MongoDB/src/app.js
+++ Backend/MongoDB/src/app.js
@@ -1,20 +1,59 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./Config/database');
-const User=require('./Models/user')
+const User = require('./Models/user')
 app.use(express.json());
 
 // signup request ko handle krega 
-app.post("/user",async(req,res)=>{
+app.post("/user", async (req, res) => {
     console.log(req.body);
-    
-    try{
+
+    try {
         await User.create(req.body);
         res.send("new user is inserted");
     }
-    catch(error){
-        res.status(404).send("something went wrong"+error);
+    catch (error) {
+        res.status(404).send("something went wrong" + error);
     }
+})
+
+// get api to fetch all user from database
+app.get("/user", async (req, res) => {
+    try {
+        const allUsers = await User.find({});
+        res.send(allUsers);
+    }
+
+    catch (error) {
+        res.status(404).send("something went wrong" + error);
+    }
+})
+
+
+// api to delete a user from database
+app.delete("/user", async (req, res) => {
+  
+  try {
+    await User.findByIdAndDelete({_id:req.body._id});
+    res.send("user is deleted");
+  }
+  catch (error) {
+    res.status(404).send("something went wrong " + error);
+  }
+})
+
+
+// api to update a particular user by  his details
+app.patch("/user",async(req,res)=>{
+    console.log(req.body.id);
+    
+  try{
+     await User.findByIdAndUpdate({_id:req.body._id},{firstName:req.body.firstName,email:req.body.email},{runValidators:true});
+     res.send("user is updated");
+  }
+  catch(error){
+    res.status(404).send("something went wrong"+error);
+  }
 })
 
 
